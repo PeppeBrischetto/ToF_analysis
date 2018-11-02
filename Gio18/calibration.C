14a6c6b71154eedcee6397e2dbb7ffef7fea6f13
Giuseppe Brischetto <peppe2087@hotmail.it>
2018-11-02 14:18:51 +0100

    Cambiata la calibrazione in energia, prendendo i centroidi con il crosshair e mettendo come errori le FWHM, stimate sempre con il crosshair

--- Gio18/calibration.C
+++ Gio18/calibration.C
@@ -8,17 +8,27 @@ void calibration() {
 
    const Int_t n = 3;
    
+   /*  ********  Questi erano i valori PRIMA di aver parlato con Politi *********  */
+   /*
    Double_t channel[n] = {1201.6, 2989., 3009.8}; // va bene per il chi-quadro
    //Double_t channel[n] = {1200.1, 2983., 3008.5}; // va bene per il log likelihood
    //Double_t channel[n] = {1201.7, 2991., 3010.3}; // valori per 4096 bin -> meglio 2048 bin
    Double_t energy[n] = {2243.796, 5419.961, 5462.863};
    Double_t err_channel[n] = {0.7, 2., 0.3};
    Double_t err_energy[n] = {0., 0., 0.};
-   
+   */
+
+   /*  ********  Questi sono i valori DOPO di aver parlato con Politi *********  */
+   const Int_t m = 2;
+   Double_t channel[m] = {1203., 3009.};  // Stimato con il crosshair al centro del bin
+   Double_t energy[m] = {2243.796, 5462.863};
+   Double_t err_channel[m] = {76, 28};
+   Double_t err_energy[m] = {0., 0.};
+
    TCanvas *c1 = new TCanvas("c1", "c1");
    //c1->SetGrid();
  
-   TGraphErrors *ch_ener = new TGraphErrors( n, energy, channel, err_energy, err_channel );
+   TGraphErrors *ch_ener = new TGraphErrors( m, energy, channel, err_energy, err_channel );
    ch_ener->SetName("Parametri del fit");
    ch_ener->SetTitle("Calibrazione in Energia");
    ch_ener->Draw("A*");
@@ -29,7 +39,7 @@ void calibration() {
    ch_ener->GetYaxis()->SetTitleSize(0.05);
    ch_ener->GetXaxis()->SetTitleOffset(0.90);
    ch_ener->GetYaxis()->SetTitleOffset(1.00);
-   ch_ener->Fit("pol1");
+   ch_ener->Fit("pol1", "M");
 
    // Questa TF1 era per provare a disegnare una retta sopra un grafico o un istogramma
    //TF1 *retta = new TF1("retta", "0.560817*x-0.", 2000., 6000.);
@@ -56,6 +66,9 @@ void calibration() {
    retta2->FixParameter(1, (retta_cal->GetParameter(1)) - (retta_cal->GetParError(1)) );
    retta2->Draw("same");
    */
+
+   retta_cal->FixParameter(0, 0.);
+   ch_ener->Fit(retta_cal, "M");
 
    gStyle->SetOptFit(0111); // con queste opzioni stampo i valori dei parametri e i loro errori
 
